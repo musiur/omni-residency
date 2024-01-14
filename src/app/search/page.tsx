d7d23f4c6ac79cad21c29ff2d1ab3bb8b729059b
MusiurAlamOpu <musiur.opu@gmail.com>
2024-01-14 20:42:17 +0600

    feature:: search: result room card added

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,21 @@
 import HeroSection from "@/components/core/pages/search/hero.section";
 import RoomCard from "@/components/core/pages/search/room.card";
+import {
+  AreaChart,
+  Baby,
+  Bath,
+  BedDouble,
+  CarFront,
+  User2,
+} from "lucide-react";
 
 const Page = () => {
   return (
     <div>
       <HeroSection />
-      <section className="container grid grid-cols-1 gap-[40px]">
-        {[1, 2, 3, 4, 5].map((item) => {
-          return <RoomCard key={item} />;
+      <section className="container grid grid-cols-1 gap-[64px]">
+        {Rooms.map((item) => {
+          return <RoomCard key={item.id} details={item} />;
         })}
       </section>
     </div>
@@ -15,3 +23,63 @@ const Page = () => {
 };
 
 export default Page;
+
+const Rooms = [
+  {
+    id: 1,
+    title: "Standard Room",
+    startFrom: 100,
+    shortDescription:
+      "All our delux room has big windows to help you take a broad view of the cityscape and nature",
+    facilities: [
+      { id: 1, icon: <User2 />, key: "adults", value: 2 },
+      { id: 2, icon: <Baby />, key: "children", value: 2 },
+      { id: 3, icon: <AreaChart />, key: "area", value: 30 },
+      { id: 4, icon: <BedDouble />, key: "bed", value: 2 },
+      { id: 5, icon: <Bath />, key: "bath", value: 1 },
+      { id: 6, icon: <CarFront />, key: "parking", value: true },
+    ],
+    viewLink: "#",
+    image: "/images/home/hero-background.png",
+    videoLink: "#",
+    photosLink: "#",
+  },
+  {
+    id: 2,
+    title: "Standard Room",
+    startFrom: 100,
+    shortDescription:
+      "All our delux room has big windows to help you take a broad view of the cityscape and nature",
+    facilities: [
+      { id: 1, icon: <User2 />, key: "adults", value: 2 },
+      { id: 2, icon: <Baby />, key: "children", value: 2 },
+      { id: 3, icon: <AreaChart />, key: "area", value: 30 },
+      { id: 4, icon: <BedDouble />, key: "bed", value: 2 },
+      { id: 5, icon: <Bath />, key: "bath", value: 1 },
+      { id: 6, icon: <CarFront />, key: "parking", value: true },
+    ],
+    viewLink: "#",
+    image: "/images/home/hero-background.png",
+    videoLink: "#",
+    photosLink: "#",
+  },
+  {
+    id: 3,
+    title: "Standard Room",
+    startFrom: 100,
+    shortDescription:
+      "All our delux room has big windows to help you take a broad view of the cityscape and nature",
+    facilities: [
+      { id: 1, icon: <User2 />, key: "adults", value: 2 },
+      { id: 2, icon: <Baby />, key: "children", value: 2 },
+      { id: 3, icon: <AreaChart />, key: "area", value: 30 },
+      { id: 4, icon: <BedDouble />, key: "bed", value: 2 },
+      { id: 5, icon: <Bath />, key: "bath", value: 1 },
+      { id: 6, icon: <CarFront />, key: "parking", value: true },
+    ],
+    viewLink: "#",
+    image: "/images/home/hero-background.png",
+    videoLink: "#",
+    photosLink: "#",
+  },
+];
--- src/components/core/pages/search/room.card.tsx
+++ src/components/core/pages/search/room.card.tsx
@@ -1,5 +1,126 @@
-const RoomCard = () => {
-  return <div>Room Card</div>;
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+import {
+  AreaChart,
+  Baby,
+  Bath,
+  BedDouble,
+  CarFront,
+  ImageIcon,
+  Play,
+  User2,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const RoomCard = ({
+  details,
+}: {
+  details: {
+    id: number;
+    image: string;
+    title: string;
+    startFrom: number;
+    viewLink: string;
+    facilities: any;
+    videoLink: string;
+    photosLink: string;
+    shortDescription: string;
+  };
+}) => {
+  const {
+    id,
+    image,
+    title,
+    startFrom,
+    viewLink,
+    facilities,
+    videoLink,
+    photosLink,
+    shortDescription,
+  } = details;
+  const even = id % 2 === 0;
+  return (
+    <div className="relative grid grid-cols-1 md:grid-cols-2 py-0 md:py-[90px] overflow-hidden">
+      <Image
+        src={image}
+        alt="hero-background-image"
+        fill
+        quality={100}
+        // style={{ objectFit: "contain", objectPosition: "left", zIndex: "-1" }}
+        className={clsx("hidden md:flex max-w-[800px] bg-contain bg-center z-[-1]", {
+          "ml-0": !even,
+          "ml-auto": even,
+        })}
+      />
+      <div
+        className={clsx(
+          "relative flex items-end gap-[10px] px-[20px] min-h-[300px] py-[24px]",
+          { "justify-start order-1": !even, "justify-end md:order-2": even }
+        )}
+      >
+        <Image
+          src={image}
+          alt="hero-background-image"
+          fill
+          quality={100}
+          style={{ objectFit: "cover", objectPosition: "center", zIndex: "-1" }}
+          className="flex md:hidden"
+        />
+        <Link
+          href={videoLink}
+          className="flex items-center justify-center gap-[8px] px-[16px] py-[8px] backdrop-blur bg-white/80 rounded-[10px]"
+        >
+          <Play className="stroke-[1.3px] w-4 h-4" />
+          Watch video
+        </Link>
+        <Link
+          href={photosLink}
+          className="flex items-center justify-center gap-[8px] px-[16px] py-[8px] backdrop-blur bg-white/80 rounded-[10px]"
+        >
+          <ImageIcon className="stroke-[1.3px] w-4 h-4" />
+          Photos
+        </Link>
+      </div>
+      <div
+        className={clsx(
+          "bg-white/90 backdrop-blur-2xl p-[16px] md:p-[40px] rounded-[10px] border flex flex-col gap-[20px]",
+          { "order-2": !even, "order-1": even }
+        )}
+      >
+        <div>
+          <p>
+            From&nbsp;
+            <span className="text-primary font-semibold">${startFrom}</span>
+            &nbsp;/night
+          </p>
+          <h3 className="text-[20px] md:text-[32px] font-semibold pt-[4px]">
+            {title}
+          </h3>
+        </div>
+        <p>{shortDescription}</p>
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-[8px] max-w-[400px]">
+          {facilities.map((item: any) => {
+            return (
+              <div
+                key={item.id}
+                className="flex items-center gap-[8px] [&>svg]:stroke-primary [&>svg]:w-4 [&>svg]:h-4 [&>svg]:stroke-[1.3px]"
+              >
+                {item.icon}
+                <span className="capitalize">
+                  {item.value}&nbsp;{item.key}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex items-center justify-between gap-[10px] pt-[16px] border-t border-gray-300">
+          <Link href={viewLink}>View details</Link>
+          <Button>Book Now</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomCard;
