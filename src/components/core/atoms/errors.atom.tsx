471b50c68d852874a2e45ae3a73e666446d193e6
musiur <musiur.opu@gmail.com>
2024-05-19 05:12:29 +0600

    all code remove

--- src/components/core/atoms/errors.atom.tsx
+++
@@ -1,8 +0,0 @@
-const Errors = ({ details }: { details: { errors: any; name: string } }) => {
-  const { errors, name } = details;
-  return errors[`${name}`] ? (
-    <div className="text-pink-600">{errors[`${name}`].message}</div>
-  ) : null;
-};
-
-export default Errors;
