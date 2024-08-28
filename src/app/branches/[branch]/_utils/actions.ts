9d3e5cc4cfd82159fbbd9c43e52373b33cf7155d
musiur <musiur.opu@gmail.com>
2024-08-28 21:03:37 +0600

    branch list action added

---
+++ src/app/branches/[branch]/_utils/actions.ts
@@ -0,0 +1,24 @@
+"use client"
+
+const BASEURL = process.env.BASEURL;
+
+
+export const A__GET__Branch = async (id: number) => {
+    try {
+        const response = await fetch(`${BASEURL}/segments/branches/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            cache: "no-store"
+        })
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        return {
+            success: false,
+            message: "Something went wrong",
+            error
+        }
+    }
+}
