18b5587fea8ee939fcb6249fbf63a2557d03d515
musiur <musiur.opu@gmail.com>
2024-08-30 19:49:19 +0600

    comment unused code

--- src/app/branches/[branch]/page.tsx
+++ src/app/branches/[branch]/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./_utils/hero.section";
 import NearbyPlaces from "./_utils/nearyby.section";
 import Overview from "./_utils/overview.section";
 import { RoomCarousel } from "./_utils/room-carousel.section";
-import Tabbar from "./_utils/tabbar.section";
+// import Tabbar from "./_utils/tabbar.section";
 import OurServices from "@/app/destination/_utils/our-services.section";
 import Facilities from "@/app/_utils/facilities.section";
 
