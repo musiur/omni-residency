7ac42e845e132ac0b100d2e5638ae95038345251
irfanhsajid <irfanhsajid@gmail.com>
2024-09-01 10:35:30 +0600

    commit for pull main branches code

--- src/app/_utils/deals-of-the-day.section.tsx
+++ src/app/_utils/deals-of-the-day.section.tsx
@@ -3,7 +3,6 @@ import { A__GET__OfferList } from "../deals-&-offers/_utils/action";
 
 const DealsOfTheDay = async () => {
   const result = await A__GET__OfferList();
-  console.log("response", result);
   return (
     <section className="container">
       <div className="flex flex-col items-center justify-center gap-[8px] pb-[32px]">
